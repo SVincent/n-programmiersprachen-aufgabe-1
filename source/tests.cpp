918a372dd989c8e00a66eed41471b86c7d776e01
SVincent <soellner.vincent@gmail.com>
2018-04-22 23:16:53 +0200

    Started task 1.8, added catch.hpp, added gcd() to test.cpp

--- source/tests.cpp
+++ source/tests.cpp
@@ -1,5 +1,23 @@
 #define CATCH_CONFIG_RUNNER
 #include <catch.hpp>
+#include <cmath>
+
+int gcd (int a, int b) { //recursive implementation
+  int result;
+  if (b == 0){
+   result = a;
+  }
+  else {
+    result = gcd(b, a%b);
+  }
+  return result;
+}
+
+TEST_CASE ("describe_gcd" , "[gcd]"){
+REQUIRE ( gcd (2 ,4) == 2);
+REQUIRE ( gcd (9 ,6) == 3);
+REQUIRE ( gcd (3 ,7) == 1);
+}
 
 int main(int argc, char* argv[])
 {
