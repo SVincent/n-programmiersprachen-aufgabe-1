ff397439b53fe661f663aef1865256e0d04f2eeb
SVincent <soellner.vincent@gmail.com>
2018-04-22 23:34:16 +0200

    Task 1.9, added checksum() and testcase for checksum()

--- source/tests.cpp
+++ source/tests.cpp
@@ -2,7 +2,7 @@
 #include <catch.hpp>
 #include <cmath>
 
-int gcd (int a, int b) { //recursive implementation
+int gcd(int a, int b){ //recursive implementation
   int result;
   if (b == 0){
    result = a;
@@ -13,10 +13,31 @@ int gcd (int a, int b) { //recursive implementation
   return result;
 }
 
-TEST_CASE ("describe_gcd" , "[gcd]"){
-REQUIRE ( gcd (2 ,4) == 2);
-REQUIRE ( gcd (9 ,6) == 3);
-REQUIRE ( gcd (3 ,7) == 1);
+TEST_CASE("describe_gcd" , "[gcd]"){
+  REQUIRE( gcd (2 ,4) == 2);
+  REQUIRE( gcd (9 ,6) == 3);
+  REQUIRE( gcd (3 ,7) == 1);
+}
+
+int checksum(int x){
+  int remainder = x;
+  int chksm = 0;
+  while (remainder != 0){
+    chksm = chksm + (remainder%10); //adds the last digit of our number to the checksum
+    if (chksm > 9) {
+      remainder = remainder / 10; 
+    }
+    else {
+      remainder = 0;
+    }
+  }
+  return chksm;
+}
+
+TEST_CASE("describe_checksum" , "[checksum]"){
+  REQUIRE(checksum(118764) == 27);
+  REQUIRE(checksum(10) == 1);
+  REQUIRE(checksum(0) == 0);
 }
 
 int main(int argc, char* argv[])
